7c19ec8fdc80fc2bf6751ebb57d1eae3c5a0ce4b
tiotdev <tiotdev@tiot.de>
2020-11-20 14:28:30 +0100

    Image bug example

--- pages/[permlink].js
+++ pages/[permlink].js
@@ -1,4 +1,5 @@
 import { useAmp } from 'next/amp';
+import Image from 'next/image';
 
 export default function Post({permlink}) {
   const isAmp = useAmp();
@@ -7,6 +8,7 @@ export default function Post({permlink}) {
     <>
       <h1>{isAmp ? 'AMP' : 'Non AMP'}</h1>
       <p>{permlink}</p>
+      {!isAmp && <Image alt="Example image" src="https://cdn.truvvle.com//fill/400/100/sm/1/aHR0cHM6Ly9pbWcudHJhdmVsZmVlZC5pby9qcHBob3RvZ3JhcGh5JTJGMjAyMDExMjBUMTAyNzA3NzQ1Wi1qcGluYXNpYWxvZ28ucG5n" width={400} height={100} />}
     </>
   );
 }
